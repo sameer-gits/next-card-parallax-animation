bc28be143ecd67e9645800fde4a434521d878946
sameer-gits <ms3054825@gmail.com>
2024-01-17 23:38:38 +0530

    finally-final-changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <main className="flex min-h-screen p-12 align-center gap-2 justify-center items-center">
       <div
         className={`transition-transform grid border-2 border-slate-800 place-content-between py-6 p-4 rounded-3xl h-fit gap-7 w- max-w-96 bg-slate-900 relative ${
-          isLeftChildHovered ? "hover:skew-y-6 " : ""
-        } ${isRightChildHovered ? "hover:-skew-y-6 " : ""}`}
+          isLeftChildHovered ? "hover:-skew-y-6 " : ""
+        } ${isRightChildHovered ? "hover:skew-y-6 " : ""}`}
       >
         <span className="absolute flex h-4 w-4 -right-2 top-6">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
@@ -42,7 +42,7 @@ export default function Home() {
         </h4>
 
         <h4
-          className="transition-all top-6 left-4 peer-hover/right:top-4 peer-hover/right:left-6 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/left:top-4 peer-hover/left:left-3"
+          className="transition-all top-6 left-4 peer-hover/left:top-4 peer-hover/left:left-6 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/right:top-4 peer-hover/right:left-3"
           style={{
             backgroundImage:
               "url(https://sameer-gits.github.io/next-card-parallax-animation/gradient.gif)",
@@ -52,7 +52,7 @@ export default function Home() {
         </h4>
 
         <h4
-          className="transition-all top-6 left-4 peer-hover/right:top-3 peer-hover/right:left-7 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/left:top-3 peer-hover/left:left-2"
+          className="transition-all top-6 left-4 peer-hover/left:top-3 peer-hover/left:left-7 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/right:top-3 peer-hover/right:left-2"
           style={{
             backgroundImage:
               "url(https://sameer-gits.github.io/next-card-parallax-animation/gradient.gif)",
@@ -62,7 +62,7 @@ export default function Home() {
         </h4>
 
         <h4
-          className="transition-all top-6 left-4 peer-hover/right:top-2 peer-hover/right:left-8 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/left:top-2 peer-hover/left:left-1"
+          className="transition-all top-6 left-4 peer-hover/left:top-2 peer-hover/left:left-8 absolute text-2xl font-bold p-4 px-12 rounded-full text-center w-fit peer-hover/right:top-2 peer-hover/right:left-1"
           style={{
             backgroundImage:
               "url(https://sameer-gits.github.io/next-card-parallax-animation/gradient.gif)",
